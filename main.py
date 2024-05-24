0cac4a2df3dcd604650e05cbe94616cba0112014
Whiiteee <soumyamishra2099@gmail.com>
2024-05-24 22:34:22 +0530

    Adding Some CSS

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-from flask import Flask,render_template,redirect
+from flask import Flask,render_template,redirect,request
 from socket import gethostname,gethostbyname
+from json import loads,dumps
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return redirect("/chat")
+def home_1():
+    return redirect("/chat/user")
 
-@app.route('/chat')
-def index():
+@app.route('/chat/')
+def home_2():
+    return redirect("/chat/user")
+
+@app.route('/chat/<path:path>',methods=["GET","POST"])
+def index(path):
+    if request.method == "POST":
+        return
     return render_template("index.html")
+
+
 
 if "__main__" == __name__:
     app.run(host=gethostbyname(gethostname()),port=80,debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,14 @@
+a = document.getElementById("send-button");
+
+
 function change_display(){
     document.getElementById("menu-bar").classList.toggle("menubar-change");
     document.getElementById("side-bar").classList.toggle("side-bar-show");
     document.getElementById("chat-container").classList.toggle("chat-container-shrink");
     
 }
+
+addEventListener("click",function(){
+    send_msg = fetch("/chat/user/send-msg");
+    print(send_msg.then())
+});
